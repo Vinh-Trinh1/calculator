891d129d11340d091ff8eb71fbc9440ba32d4738
Vinh-Trinh1 <vinhtrinh0113@gmail.com>
2026-01-04 19:27:19 -0500

    changed where numbers are stored into a stack. No more arrayList in file. Need to rethink doOperation.

--- Output.java
+++ Output.java
@@ -5,13 +5,13 @@
 import java.util.Scanner; 
 import java.util.regex.Pattern; // for regular expressions, to detect numbers and operations
 import java.util.regex.Matcher; // for regular expressions, to search for numbers or operations in the string
-import java.util.ArrayList; 
+import java.util.Stack; // creates the stack object from java 
 
 public class Output{
     private String expression; // creates a string that holds the expression e.x "2+2x3" 
     private Arithmetic arithmetic; 
-    private ArrayList<Integer> numbers = new ArrayList<Integer>(); 
-
+    private Stack<Integer> numStack; 
+    private Stack<String> operationStack; 
     public static void main(String[] args) {
         Output o = new Output();
         o.userInput(); 
@@ -20,6 +20,8 @@ public class Output{
     public Output(){
         expression = ""; 
         arithmetic = new Arithmetic(); 
+        numStack = new Stack<Integer>(); 
+        operationStack = new Stack<String>(); 
     }
 
     // gets the expression from the user and add it to expression 
@@ -31,7 +33,8 @@ public class Output{
         while(endLoop){
             String operation = obj.nextLine(); 
             expression += operation; 
-                if (operation.replaceAll("\\s+", " ").equals("=")){ endLoop = false; }
+                if (operation.replaceAll("\\s+", " ").equals("=")){ endLoop = false; } 
+                else if(operation.contains("=")) { endLoop=false; }
             }
         }
 
@@ -49,18 +52,34 @@ public class Output{
         while(numberMatcher.find()){
             String numberStr = numberMatcher.group(); 
             // adds the number into an array 
-            numbers.add(Integer.parseInt(numberStr)); 
+            numStack.push(Integer.parseInt(numberStr)); 
         }
 
         // iterates through the string checking for operations
         while(operationMatcher.find()){
             String s = operationMatcher.group(); 
+            operationStack.push(s); 
             this.doOperation(s); 
         }
     }
 
     // checks which operation the user wants to do from getOperation 
     public void doOperation(String s) {
+        if(operationStack.empty()){
+            return; 
+        }
+        if(s.equals("(")){
+            if(s.equals("+")){
+            arithmetic.add(2, 1); 
+            } else if(s.equals("-")){
+                arithmetic.subtract(); 
+            } else if(s.equals("*")){
+                arithmetic.multiplication();
+            } else if (s.equals("/")){
+                arithmetic.division(); 
+            }
+        }
+        
         if(s.equals("+")){
             arithmetic.add(1, 2); 
         } else if(s.equals("-")){
